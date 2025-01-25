e0e3ca9506f41fe017b5a292ad9680c08c4c4bf6
Dattatray8 <dattatraymahindrakar@gmail.com>
2025-01-25 19:58:18 +0530

    Simple age calculator

---
+++ age-calculator.py
@@ -0,0 +1,10 @@
+num = int(input("Enter a number(0-9):"))
+num=num*2
+num=num+5
+num=num*50
+num=num+1774
+year=int(input("Enter your birth year(in format YYYY):"))
+num=num-year
+num=str(num)
+print("Your age is:",num[1:])
+
